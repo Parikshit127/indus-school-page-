6350bf6e2c37ea86fa8b1c21477b91bf24a91433
visheshsingal <vishesh.singal.contact@gmail.com>
2025-12-17 11:53:38 +0530

    added images in life at indus

--- frontend/src/components/Gallery.tsx
+++ frontend/src/components/Gallery.tsx
@@ -2,10 +2,10 @@ import { Section } from "@/components/ui/section";
 
 export function Gallery() {
     const images = [
-        "https://images.unsplash.com/photo-1525921429624-479b6a26d84d?q=80&w=2070&auto=format&fit=crop", // Campus
-        "https://images.unsplash.com/photo-1509062522246-3755977927d7?q=80&w=2132&auto=format&fit=crop", // Students
-        "https://images.unsplash.com/photo-1588072432836-e10032774350?q=80&w=2072&auto=format&fit=crop", // Library
-        "https://images.unsplash.com/photo-1577896851231-70ef18881754?q=80&w=2070&auto=format&fit=crop", // Sports
+        "https://content3.jdmagicbox.com/v2/comp/rohtak/dc/9999p1262.1262.1239634669u5i3c7.dc/catalogue/indus-public-school-rohtak-ho-rohtak-cbse-schools-cbq9kft3pk.jpg", // Campus
+        "https://content3.jdmagicbox.com/v2/comp/rohtak/dc/9999p1262.1262.1239634669u5i3c7.dc/catalogue/indus-public-school-rohtak-ho-rohtak-cbse-schools-bpfwwzqkt4.jpg", // Students
+        "https://content.jdmagicbox.com/comp/rohtak/dc/9999p1262.1262.1239634669u5i3c7.dc/catalogue/indus-public-school-rohtak-ho-rohtak-cbse-schools-08c4a863u0.jpg", // Library
+        "https://content3.jdmagicbox.com/v2/comp/rohtak/dc/9999p1262.1262.1239634669u5i3c7.dc/catalogue/indus-public-school-rohtak-ho-rohtak-cbse-schools-9u4315q9gp.jpg", // Sports
     ];
 
     return (
